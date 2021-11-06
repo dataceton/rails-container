d56ad24a2d194453217ff1905d5782b34e93a54b
Daniil Davydov <daniil.davydov@rt.ru>
2021-11-06 21:52:42 +0300

    Add auto registration with zeitwerk

--- lib/rails_container/container.rb
+++ lib/rails_container/container.rb
@@ -17,5 +17,22 @@ module RailsContainer
     def container
       self.class.instance
     end
+
+    def auto_register!(path)
+      Dir[File.join(Dir.pwd, path, '**/*')].each do |element|
+        next unless element.include?('.rb')
+
+        file_name = element.match(/(?<=#{path}\/).+/)[0][0...-3]
+        container_key = file_name.gsub('/', '.')
+
+        Rails.autoloaders.main.on_load(file_name.camelize) do |klass, _abspath|
+          if block_given?
+            container.register(container_key) { yield klass }
+          else
+            container.register(container_key) { klass }
+          end
+        end
+      end
+    end
   end
 end
